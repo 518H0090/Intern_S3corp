bf707e304d87d9e8b717e557b445843e00f20ccc
Hieu Huynh <hieu.huynh@s3corp.com.vn>
2022-08-24 17:25:50 +0700

    Test Docker for Dotnet

---
+++ Day28/TestDockerForWebApi/TestDockerForWebApi/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestDockerForWebApi
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+
+        public DbSet<Student> Students { set; get; }
+    }
+}
---
+++ Day28/TestDockerForWebApi/TestDockerForWebApi/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestDockerForWebApi
+{
+    public class Student
+    {
+        [Key]
+        public int Id { set; get; }
+
+        [Required]
+        public string StudentName { set; get; }
+
+        [Required]
+        public string StudentClass { set; get; }
+    }
+}
